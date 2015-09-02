576a5de6dca13abb2339944d5a70c354fa756258
Roger Humphreys <rhumfloripa@gmail.com>
2015-09-02 10:10:10 -0300

    1st commit RealFingerRunner

---
+++ RFRunner/RealFingerRunner/Assets/Resources/Scripts/MovingThetexture.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingThetexture : MonoBehaviour {
+	public float speed;
+	private float offSet;
+	public Material currentMaterial;
+	//public currentMaterial renderer.material; 
+	
+	// Use this for initialization
+	void Start () {
+		currentMaterial = GetComponent<Renderer>().material;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		offSet += speed * Time.deltaTime;
+		
+		currentMaterial.SetTextureOffset("_MainTex",new Vector2(0,offSet));
+	}
+	
+}
