7713bc7c80e81ac4407c2c668b49e73bd82dde5b
Roger Humphreys <rhumfloripa@gmail.com>
2015-11-25 19:34:50 -0200

    fix bugs in mobile scripts

--- RFRunner/RealFingerRunner/Assets/Scripts/Colisao.cs
+++ RFRunner/RealFingerRunner/Assets/Scripts/Colisao.cs
@@ -8,57 +8,53 @@ public class Colisao : MonoBehaviour
 
 	AudioSource audioFinish;
 
-    float myscore;
-    float highScore;
-    public static bool isRecord;
+	float myscore;
+	float highScore;
+	public static bool isRecord;
 	public static string key = "score"; // nome da chave que ira gravar
-    public AudioClip win;
-    public AudioClip lose;
+	public AudioClip win;
+	public AudioClip lose;
 
-    //ArrayList lNames = new ArrayList ();
-    //public InputField name;
+	//ArrayList lNames = new ArrayList ();
+	//public InputField name;
 
 
-    void Start ()
+	void Start ()
 	{
-        audioFinish = GetComponent<AudioSource> ();
-        audioFinish.clip = lose;
-        isRecord = false;
-    }
+		audioFinish = GetComponent<AudioSource> ();
+		audioFinish.clip = lose;
+		isRecord = false;
+	}
 	
 
-    void OnTriggerEnter(Collider collider)
-    {
-        if (collider.gameObject.tag == "Player" && !SwipeDetector.queimouLargada)
-        {
-            Score.gameFinished = true;
-            myscore = Score.startTime;
+	void OnTriggerEnter (Collider collider)
+	{
+		if (collider.gameObject.tag == "Player" && !SwipeDetector.queimouLargada) {
+			Score.gameFinished = true;
+			myscore = Score.startTime;
             
-            //Grava o score
-            if (PlayerPrefs.HasKey(key))
-            {
-                highScore = PlayerPrefs.GetFloat(key);
+			//Grava o score
+			if (PlayerPrefs.HasKey (key)) {
+				highScore = PlayerPrefs.GetFloat (key);
 
-                if (myscore < highScore)
-                {
-                    isRecord = true;
-                    PlayerPrefs.SetFloat(key, myscore);
-                    highScore = myscore;   
-                    audioFinish.clip = win;
-                    PlayerPrefs.Save();
-                } 
-            }
-            else
-            {
-                // se nao tem save, ele cria agora!!!
-                PlayerPrefs.SetFloat(key, myscore);
-                PlayerPrefs.Save();
-            }
+				if (myscore < highScore) {
+					isRecord = true;
+					PlayerPrefs.SetFloat (key, myscore);
+					highScore = myscore;   
+					audioFinish.clip = win;
+					PlayerPrefs.Save ();
+				} 
+			} else {
+				// se nao tem save, ele cria agora!!!
+				PlayerPrefs.SetFloat (key, myscore);
+				audioFinish.clip = win;
+				PlayerPrefs.Save ();
+			}
 
-            audioFinish.Play();
+			audioFinish.Play ();
 
-            //ADICAO DE NOME E ARRAY LISTA COM SAVE
-            //name.text = "Enter your name :";
-        }
-    }
+			//ADICAO DE NOME E ARRAY LISTA COM SAVE
+			//name.text = "Enter your name :";
+		}
+	}
 }
--- RFRunner/RealFingerRunner/Assets/Scripts/SwipeDetector.cs
+++ RFRunner/RealFingerRunner/Assets/Scripts/SwipeDetector.cs
@@ -31,44 +31,51 @@ public class SwipeDetector : MonoBehaviour
 
 		accelarator = 0;
 		counter = 0;
-    }
+	}
 
-    void SetSpeed()
-    {
-        if (accelarator >= 58) { accelarator = 70; return; }
-        else if (accelarator >= 36) { accelarator = 60; return; }
-        else if (accelarator >= 26) { accelarator = 40; return; }
-        else if (accelarator >= 18) { accelarator = 30; return; }
-        else if (accelarator < 15) { accelarator = 23; }
+	void SetSpeed ()
+	{
+		if (accelarator >= 58) {
+			accelarator = 70;
+			return;
+		} else if (accelarator >= 36) {
+			accelarator = 60;
+			return;
+		} else if (accelarator >= 26) {
+			accelarator = 40;
+			return;
+		} else if (accelarator >= 18) {
+			accelarator = 30;
+			return;
+		} else if (accelarator < 15) {
+			accelarator = 23;
+		}
 	}
 
 
 	void Update ()
 	{
-        if (!Score.startGame)
-        {
+		if (!Score.startGame) {
 			camera.transform.rotation = Quaternion.Slerp (camera.transform.rotation, Quaternion.Euler (25, 0, 0), .25f);
 			camera.transform.position = Vector3.Lerp (camera.transform.position,
                     new Vector3 (camera.transform.position.x, 10, camera.transform.position.z), .075f);
 		}
 
-		if (startRunning)
-        {
+		if (startRunning) {
 			camera.transform.position = Vector3.Lerp (camera.transform.position,
                     new Vector3 (camera.transform.position.x, 13, camera.transform.position.z), .01f);
 			camera.transform.rotation = Quaternion.Slerp (camera.transform.rotation, Quaternion.Euler (xzRotation, 0, xzRotation), .1f);
 
-			rgbody.velocity = new Vector3 (0, 0, accelarator); //mobile precisa add *Time.deltatime
+			rgbody.velocity = new Vector3 (0, 0, accelarator * Time.deltaTime); //mobile precisa add *Time.deltaTime
 		}
 
-        if (accelarator > 0)
-            accelarator -= accelarator * .016f; //desacelara total em 1 seg (1.6% ao frame)
+		if (accelarator > 0)
+			accelarator -= accelarator * .0016f; //desacelara total em 1 seg (1.6% ao frame)
 
 #if UNITY_EDITOR
 
-        if (Input.GetKeyDown (KeyCode.Space))
-        {
-			SetSpeed();
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			SetSpeed ();
 			xzRotation *= -1;
 			startRunning = true;
 			audioStep.Play ();
@@ -167,5 +174,5 @@ public class SwipeDetector : MonoBehaviour
 		}
 	}
 #endif
-    }
+	}
 }
